ea7fb8355ba3d7112e10e2364e4647e8668a7145
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-03-23 15:13:04 -0400

    Add redis to index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ const redis = require("redis");
 
 const app = express();
 const client = redis.createClient();
+client.set("visits", 0);
+
+// router
+app.get("/", (req, res) => {
+    client.get("visits", (err, visits) => {
+        res.send(visits + " visits so far.");
+        client.set("visits", +visits++);
+    });
+});
+
+app.listen(8080);
